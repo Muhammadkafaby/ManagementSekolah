5db1b60ed3106e8edaa8389c8a80a12ab3d0ebf6
Muhammadkafaby <kafaby@student.telkomuniversity.ac.id>
2023-11-23 07:25:50 +0700

    force https

--- database/migrations/2021_08_04_141828_create_sessions_table.php
+++ database/migrations/2021_08_04_141828_create_sessions_table.php
@@ -14,14 +14,12 @@ class CreateSessionsTable extends Migration
     public function up()
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->id()->unsigned();
-            $table->foreignId('user_id')->nullable()->constrained();
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
-            $table->dateTime('last_activity')->index();
-
-            // SQLSTATE[42000]: Syntax error or access violation: 1068 Multiple primary key defined (SQL: alter table `sessions` add primary key `sessions_id_primary`(`id`))
+            $table->timestamp('last_activity')->index();
 
             
         });
