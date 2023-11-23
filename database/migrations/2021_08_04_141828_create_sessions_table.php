4ef35e9d95c9cddd06c6bdbb1eb88ab8ee76e905
Muhammadkafaby <kafaby@student.telkomuniversity.ac.id>
2023-11-23 10:28:48 +0700

    force https

--- database/migrations/2021_08_04_141828_create_sessions_table.php
+++ database/migrations/2021_08_04_141828_create_sessions_table.php
@@ -14,14 +14,12 @@ class CreateSessionsTable extends Migration
     public function up()
     {
         Schema::create('sessions', function (Blueprint $table) {
-            $table->id();
+            $table->string('id')->primary();
             $table->foreignId('user_id')->nullable()->index();
             $table->string('ip_address', 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->text('payload');
-            $table->timestamp('last_activity')->index();
-
-            
+            $table->integer('last_activity')->index();
         });
     }
 
